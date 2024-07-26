c312ef70a93f51f40e0677578ac85b112507f2cc
Anubhav <mr.anubhavtiwary@gmail.com>
2024-07-26 22:58:54 +0530

    added register, toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,25 +5,41 @@ import {
   Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={
-          <Layout>
-            <p>Home Page</p>
-          </Layout>} 
+        <Route
+          path='/'
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          }
         />
-        <Route path="/search" element={
-          <Layout>
-            <p>Home Page</p>
-          </Layout>}
+        <Route
+          path='/search'
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          }
         />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route
+          path='/register'
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
+
+        <Route path='*' element={<Navigate to='/' />} />
       </Routes>
     </Router>
   );
-}  
+};
 
 export default App;
