264946746523bca4dc232dd5bcea5a3f49b30965
Anubhav <mr.anubhavtiwary@gmail.com>
2024-07-26 21:02:15 +0530

    added authentication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import mongoose from 'mongoose';
-import userRoutes from './routes/users';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -11,10 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
-    console.log("server is running");
-})
+  console.log("server is running");
+});
 
 // npm run dev
