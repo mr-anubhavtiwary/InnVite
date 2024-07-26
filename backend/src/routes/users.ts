0a31566afb293accd8671bbc534074177fa6e272
Anubhav <mr.anubhavtiwary@gmail.com>
2024-07-27 00:30:50 +0530

    fixing basic errors

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import User from "../models/user";
 import jwt from "jsonwebtoken";
 import { check, validationResult } from "express-validator";
+import verifyToken from "../middleware/auth";
 
 const router = express.Router();
 
@@ -10,8 +11,8 @@ router.post(
   "/register",
   [
     check("firstName", "First Name is required").isString(),
-    check("lastName", "Last name is required").isString(),
-    check("email", "email is required").isEmail(),
+    check("lastName", "Last Name is required").isString(),
+    check("email", "Email is required").isEmail(),
     check("password", "Password with 6 or more characters required").isLength({
       min: 6,
     }),
@@ -26,6 +27,7 @@ router.post(
       let user = await User.findOne({
         email: req.body.email,
       });
+
       if (user) {
         return res.status(400).json({ message: "User already exists" });
       }
@@ -44,9 +46,9 @@ router.post(
       res.cookie("auth_token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        maxAge: 86400000, //in ms
+        maxAge: 86400000,
       });
-      return res.sendStatus(200).send({ message: "User registered OK" });
+      return res.status(200).send({ message: "User registered OK" });
     } catch (error) {
       console.log(error);
       res.status(500).send({ message: "Something went wrong" });
