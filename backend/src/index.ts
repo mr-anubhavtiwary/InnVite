00a6075a2aeb79107871672d8a925b3609631c41
Anubhav <mr.anubhavtiwary@gmail.com>
2024-07-26 20:18:38 +0530

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+import userRoutes from './routes/users';
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/api/users", userRoutes);
+
+app.listen(7000, () => {
+    console.log("server is running");
+})
+
+// npm run dev
---
+++ frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Layout from "./layouts/Layout";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <Layout>
+            <p>Home Page</p>
+          </Layout>} 
+        />
+        <Route path="/search" element={
+          <Layout>
+            <p>Home Page</p>
+          </Layout>}
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+}  
+
+export default App;
