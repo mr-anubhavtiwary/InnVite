e3ecba3a153833f83c9b036b45505e5646d0fd82
Anubhav <mr.anubhavtiwary@gmail.com>
2024-07-27 16:00:31 +0530

    add hotels feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,16 @@ import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+import myHotelRoutes from "./routes/my-hotels";
+// import hotelRoutes from "./routes/hotels";
+// import bookingRoutes from "./routes/my-bookings";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -24,9 +34,16 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
+// app.use("/api/hotels", hotelRoutes);
+// app.use("/api/my-bookings", bookingRoutes);
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
 app.listen(7000, () => {
-  console.log("server is running");
+  console.log("server running on localhost:7000");
 });
 
 // npm run dev
